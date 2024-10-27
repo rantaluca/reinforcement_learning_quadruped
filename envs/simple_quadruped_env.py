d5e8e2e482a700594141f8c035e3a9da37470c8c
Robert Antaluca <robert@antaluca.com>
2024-10-27 21:46:25 +0100

    finished first env

--- envs/simple_quadruped_env.py
+++ envs/simple_quadruped_env.py
@@ -26,6 +26,7 @@ class QuadrupedEnv(gym.Env):
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(51,), dtype=np.float32)  #Observation: state + LIDAR + IMU + Pose Estimation + Pose Target
 
         self.time_on_ground = 0
+        self.treshold_target = 0.1
         
         p.connect(p.GUI)  # or p.DIRECT non-graphical version
         p.setAdditionalSearchPath(pybullet_data.getDataPath()) #Pybullet models library
@@ -38,7 +39,7 @@ class QuadrupedEnv(gym.Env):
         p.resetSimulation()
         p.setGravity(0, 0, -9.81)
         p.loadURDF("plane.urdf")
-        self.robot_id = p.loadURDF("your_quadruped.urdf", [0, 0, 0.5])
+        self.robot_id = p.loadURDF("/ressources/urdfs/spot/spot_v1.urdf", [0, 0, 0.5])
         self.time_on_ground = 0
         # Adding the lidar
         self.lidar_joint_index = p.createConstraint(self.robot_id, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0.5])
@@ -54,19 +55,68 @@ class QuadrupedEnv(gym.Env):
         truncated = False
         done = False
         obs = self._get_observation()
-        reward = self.compute_reward(p.getBasePositionAndOrientation(self.robot_id)[0], self.target_position)
+        reward = self.compute_reward(p.getBasePositionAndOrientation(self.robot_id), self.target_position)
         #If the robot reach the point ot falls the episode is terminated
         contact_points = p.getContactPoints(bodyA=self.robot_id, linkIndexA=-1)
-        #if the robot is on the ground for too long, the episode is terminated
+        #If the robot is on the ground for too long, the episode is terminated
         if contact_points:
             self.time_on_ground += 1
             if self.time_on_ground > 150:
                 done = True
                 truncated = True
-        #if the robot is close enough to the target (orientation and position), the episode is terminated
-        if np.linalg.norm(np.array(p.getBasePositionAndOrientation(self.robot_id)[0]) - np.array(self.target_position)) < 0.1 and np.linalg.norm(np.array(p.getBasePositionAndOrientation(self.robot_id)[1]) - np.array(self.target_orientation)) < 0.1:
+
+        position, orientation = p.getBasePositionAndOrientation(self.robot_id)
+        #Converting to euler 
+        robot_orientation_euler = p.getEulerFromQuaternion(orientation)
+
+        # Checking if position and orientation are close enough to the target
+        if (np.linalg.norm(np.array(position) - np.array(self.target_position)) < self.treshold_target and
+            np.linalg.norm(np.array(robot_orientation_euler) - np.array(self.target_orientation)) < self.treshold_target):
             done = True
+
         return obs, reward, done, truncated, {}
+    
+    def _get_observation(self):
+        #Returns the  the joint positions, joint velocities, linear and angular velocity from the imu and data from the lidar
+        joint_states = p.getJointStates(self.robot_id, range(p.getNumJoints(self.robot_id)))
+        joint_positions = [state[0] for state in joint_states]
+        joint_velocities = [state[1] for state in joint_states]
+        #Lidar and IMU 
+        lidar_data = self._simulate_lidar()
+        imu_data = self._simulate_imu()
+        position, orientation = p.getBasePositionAndOrientation(self.robot_id) #to be commented if no position in observation
+        return np.array(joint_positions + joint_velocities + lidar_data + imu_data + list(position) + list(orientation) + self.target_position + self.target_orientation)
+        #return np.array(joint_positions + joint_velocities + lidar_data + imu_data + self.target_position + self.target_orientation)
+
+    def _simulate_lidar(self):
+        #Simulates the lidar data
+        lidar_data = []
+        for i in range(120):
+            ray_from = [0, 0, 0.5]
+            ray_to = [4*np.sin(i * np.pi / 60), 4*np.cos(i * np.pi / 60), 0.5]
+            result = p.rayTest(ray_from, ray_to)
+            lidar_data.append(result[0])
+        return lidar_data
+    
+    def _simulate_imu(self):
+        #Simulates the imu data
+        base_velocity, base_angular_velocity = p.getBaseVelocity(self.robot_id)
+        imu_data = list(base_velocity) + list(base_angular_velocity)
+        return imu_data
+    
+    def compute_reward(self, position, target_position):
+        #Computes the reward based on the distance to the target position
+        distance = np.linalg.norm(np.array(position) - np.array(target_position))
+        return -distance - self.time_on_ground * 0.4
+    
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        p.disconnect()
+
+    
+
     
 
 
